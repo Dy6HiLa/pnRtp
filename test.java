84b8cc1c8c6d758a3b623c27e93109620ec1dd1c
PrivateNull <xsander2000dota@gmail.com>
2026-07-09 01:23:09 +0300

    Release v1.0.1: RGB/hex colors, prefix, instant teleport, near player logic fix

---
+++ test.java
@@ -0,0 +1,8 @@
+import org.bukkit.Registry;
+import org.bukkit.NamespacedKey;
+public class Test {
+    public static void main(String[] args) {
+        Object a = Registry.SOUNDS;
+        Object b = Registry.PARTICLE_TYPE;
+    }
+}
